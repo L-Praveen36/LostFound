eb2af17b003787716a2b95cff42f6294ad3e71ce
Praveen <lunavathpraveen36@gmail.com>
2025-06-13 17:33:28 +0530

    Final changes before deployment

--- src/components/ReportForm.js
+++ src/components/ReportForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ReportForm = ({ showForm, onClose }) => {
   const [formData, setFormData] = useState({
