be509fe39602797a87021b300488523c8c3dcb0a
Praveen <lunavathpraveen36@gmail.com>
2025-06-26 21:06:25 +0530

    Final changes before deployment

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthProvider } from './AuthContext'; // ✅ Import AuthProvider
 import 'react-toastify/dist/ReactToastify.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>           {/* ✅ Wrap App with AuthProvider */}
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
