be7c707a436c3612636e3d63cf0aca4e16f078be
Praveen <lunavathpraveen36@gmail.com>
2025-07-16 12:08:31 +0530

    Final changes before deployment

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -89,20 +89,21 @@ function Listings() {
 
         <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-4 mb-10">
           
-  <input
-    type="text"
-    placeholder="Search by title, location..."
-    className="w-full md:w-2/3 px-4 py-2 rounded-lg border text-gray-700 border-gray-400"
-    value={search}
-    onChange={(e) => setSearch(e.target.value)}
-  />
-  <input
-    type="date"
-    className="w-full md:w-1/3 px-4 py-2 rounded-lg border text-gray-700 border-gray-400"
-    value={searchDate}
-    onChange={(e) => setSearchDate(e.target.value)}
-  />
-
+   <div className="flex flex-col md:flex-row gap-4 mb-6">
+            <input
+              type="text"
+              placeholder="Search by title, location, email..."
+              className="w-full md:w-2/3 px-4 py-2 rounded-lg bg-white bg-opacity-10 text-white placeholder-white placeholder-opacity-70 focus:outline-none"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <input
+              type="date"
+              className="w-full md:w-1/3 px-4 py-2 rounded-lg bg-white bg-opacity-10 text-white placeholder-white placeholder-opacity-70 focus:outline-none"
+              value={searchDate}
+              onChange={(e) => setSearchDate(e.target.value)}
+            />
+          </div>
 
           <div className="flex flex-wrap gap-2">
             {FILTER_TYPES.map(type => (
